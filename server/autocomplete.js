1d80805af9699a1b37bb2f2653d3cbeca111c5a5
Alim Amanzholov <alim.amanzholov@gmail.com>
2020-01-23 10:43:10 +0900

    made the code synchronous with async/awaits

--- server/autocomplete.js
+++ server/autocomplete.js
@@ -1,3 +1,5 @@
+/*eslint-disable global-require*/
+
 'use strict';
 
 const mysql = require("mysql");
@@ -5,30 +7,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 //establishing connection to the database
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+const connectDB = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+  });
 
-connection.connect((err) => {
-  if (err) throw err;
+  await connection.connect()
   console.log("Connected to DB");
-});
 
-global.db = connection;
+  global.db = connection;
+}
 
-//setting up the server endpoint
-const app = express();
+connectDB().then(() => {
+  //setting up the server endpoint
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-//eslint-disable-next-line no-undef
-require('./routes/routes.js')(app, db);
+  //eslint-disable-next-line no-undef
+  require('./routes/routes.js')(app, db);
 
-app.listen(3000, (err) => {
-  if (err) throw err;
-  console.log("Server listening");
+  app.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("Server listening");
+  });
+}).
+catch((err) => {
+  throw err;
 });
